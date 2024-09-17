04899542b0fd650ee00af96e733085467a46e93d
DisableGraphics <elchifladod@gmail.com>
2024-09-17 14:30:55 +0200

    Menu de pausa

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+	public Text text;
+	bool is_paused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public void OnPauseClicked() {
+		if(is_paused) {
+			Time.timeScale = 1;
+			text.text = "Pause (Esc)";
+			is_paused = false;
+		} else {
+			Time.timeScale = 0;
+			text.text = "Resume (Esc)";
+			is_paused = true;
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+			OnPauseClicked();
+    }
+}
